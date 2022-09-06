46ec1e5c6df36382af21e78d5a3ef33c8cacde12
Antonio J. Fdez <antonio-jesus.fernandez-amador@t-systems.com>
2022-09-06 15:24:49 +0200

    Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
-//import { FlexLayoutModule } from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -23,7 +23,7 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatListModule,
-//    FlexLayoutModule,
+    FlexLayoutModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule
