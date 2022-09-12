d1265fbbd2dc25c0efe559d1a2612ee883c4a92b
Antonio J. Fdez <antonio-jesus.fernandez-amador@t-systems.com>
2022-09-12 14:17:00 +0200

    Basic Service

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
 
-  dishes: Dish[] = DISHES;
+export class MenuComponent implements OnInit {
+
+  dishes: Dish[] = [];
 
   selectedDish: Dish = this.dishes[0];
 
-  constructor() { }
+  constructor(private dishService: DishService) {  }
+
+  ngOnInit(): void {
+    this.dishes = this.dishService.getDishes();
+  }
 
   onSelect(dish: Dish) {
     this.selectedDish = dish;
