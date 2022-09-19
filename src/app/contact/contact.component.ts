75a227bb7f676cabb70730c5142e5a8f952cd329
Antonio J. Fdez <antonio-jesus.fernandez-amador@t-systems.com>
2022-09-19 16:31:30 +0200

    Reactive Forms Part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,27 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ControlConfig, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
+
+interface validationMessages {
+  firstname: {
+    required: string,
+    minlength: string,
+    maxlength: string
+  },
+  lastname: {
+    required: string,
+    minlength: string,
+    maxlength: string
+  },
+  telnum: {
+    required: string,
+    pattern: string
+  },
+  email: {
+    required: string,
+    email: string
+  }
+}
 
 @Component({
   selector: 'app-contact',
@@ -14,6 +35,36 @@ export class ContactComponent implements OnInit {
   contactType = ContactType;
   @ViewChild('fform') feedbackFormDirective: any;
 
+  temp: string = '';
+
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  }
+
+  validationMessage: validationMessages = {
+    'firstname': {
+      'required': 'First name is required.',
+      'minlength': 'First name must be at least 2 characters long',
+      'maxlength': 'First name cannot be more than 25 characters long'
+    },
+    'lastname': {
+      'required': 'Last name is required.',
+      'minlength': 'Last name must be at least 2 characters long',
+      'maxlength': 'Last name cannot be more than 25 characters long'
+    },
+    'telnum': {
+      'required': 'Tel. number is required.',
+      'pattern': 'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required': 'Email is required.',
+      'email': 'Email not in the valid format.'
+    }
+  }
+
   constructor(private fb: FormBuilder) {
     this.createForm();
    }
@@ -23,14 +74,39 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [null, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [null, [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        this.formErrors[field as keyof typeof this.formErrors] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessage[field as keyof validationMessages];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field as keyof typeof this.formErrors] += messages[key as keyof typeof messages] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
   onSubmit(){
